054df1a26ebf183fd15810ff4ef7effebbff087a
phntmz <hieuminhnguyen1273@gmail.com>
2021-10-25 10:50:12 +0700

    Revert "new branch > dark and light theme time reveresed"
    This reverts commit f5a9f1966de9928491dca18d05df0c0e4304eae2.

--- hello/static/js/darkmode.js
+++ hello/static/js/darkmode.js
@@ -5,9 +5,9 @@ function setThemePreference() {
     console.log(currentHour)
 
     if (currentHour >= 18 || currentHour <= 6) {
-        document.body.setAttribute("data-theme", "light_theme")
-    } else {
         document.body.setAttribute("data-theme", "dark_theme")
+    } else {
+        document.body.setAttribute("data-theme", "light_theme")
     }
 }
 
