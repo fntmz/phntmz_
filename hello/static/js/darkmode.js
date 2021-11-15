de5835cae800f018959c47dae146f45e86ee7cbc
phntmz <hieuminhnguyen1273@gmail.com>
2021-11-15 12:04:59 +0700

    a lot of stuff. read changelog.

--- hello/static/js/darkmode.js
+++ hello/static/js/darkmode.js
@@ -6,10 +6,10 @@ function setThemePreference() {
 
     // changes the website theme to dark at 6pm and back at 6am
     if (currentHour >= 18 || currentHour <= 6) {
-        document.body.setAttribute("data-theme", "dark_theme")
+        document.body.setAttribute("data-theme", "light_theme")
     }
     else {
-        document.body.setAttribute("data-theme", "light_theme")
+        document.body.setAttribute("data-theme", "dark_theme")
     };
 
     // remove the gallery at any time besides midnight
