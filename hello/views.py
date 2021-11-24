05d0b6b4800ea490935fc0b67004c8efedafbbf7
phntmz <hieuminhnguyen1273@gmail.com>
2021-11-24 08:27:35 +0700

    removed text selection from loading elements

--- hello/views.py
+++ hello/views.py
@@ -7,6 +7,7 @@ from .models import Greeting
 
 context = {
     "changelog": [
+        {"date": "24.11.2021", "detail": "removed text selection on the loading element"},
         {"date": "23.11.2021", "detail": "fixed a bug with the custom css cursor on the gallery page that causes the cursor to move along with the page while scrolling"},
         {"date": "21.11.2021",
             "detail": "changed the animation of the third and fourth button on the gallery page."},
