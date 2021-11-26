1e642bfa0f2faaa27802befaf70589b35e79afce
phntmz <hieuminhnguyen1273@gmail.com>
2021-11-26 20:35:04 +0700

    animation abuse

--- hello/static/js/navbar.js
+++ hello/static/js/navbar.js
@@ -44,14 +44,48 @@ function openNav() {
 }
 
 function closeNav() {
-  $("#myNav").css("right", "-100vw");
+  $(".closebtn").css("opacity", "0")
+
   setTimeout(
     function () {
-      $("#navbar-item-1, #navbar-item-2, #navbar-item-3, #navbar-item-4").css("bottom", "-10px")
-      $("#navbar-brand").css({ "left": "-60px", "opacity": "0" })
-      $(".navbar-item").css("opacity", "0")
       $("#navbar-socials").css("opacity", "0")
-      $(".closebtn").css({ "top": "100vh", "opacity": "0" })
-    }, 500
+    }, 400
   )
+  setTimeout(
+    function () {
+      $("#navbar-item-5").css({ "opacity": "0", "bottom": "-10px" })
+    }, 700
+  )
+  setTimeout(
+    function () {
+      $("#navbar-item-4").css({ "opacity": "0", "bottom": "-10px" })
+    }, 900
+  )
+  setTimeout(
+    function () {
+      $("#navbar-item-3").css({ "opacity": "0", "bottom": "-10px" })
+    }, 1100
+  )
+  setTimeout(
+    function () {
+      $("#navbar-item-2").css({ "opacity": "0", "bottom": "-10px" })
+    }, 1300
+  )
+  setTimeout(
+    function () {
+      $("#navbar-item-1").css({ "opacity": "0", "bottom": "-10px" })
+    }, 1500
+  )
+  setTimeout(
+    function () {
+      $("#navbar-brand").css({ "opacity": "0", "left": "0" })
+    }, 1800
+  )
+  setTimeout(
+    function () {
+      $("#myNav").css("right", "-100vw");
+      $(".closebtn").css("top", "100vh")
+    }, 2000
+  )
+
 }
