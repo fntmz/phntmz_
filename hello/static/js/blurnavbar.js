d761689ef77431bf9cf9c2087c3bd776e577fb54
Hieu Minh Nguyen <hieuminhnguyen1273@gmail.com>
2022-08-28 16:01:05 +0700

    performance improvement on firefox

--- hello/static/js/blurnavbar.js
+++ hello/static/js/blurnavbar.js
@@ -2,4 +2,5 @@ if (localStorage.getItem("blur") == "true") {
     $(".overlay").css("backdrop-filter", "blur(5px)");
     $(".overlay").css("background", "transparent");
     $("#settings-wrapper").css("background", "transparent");
+    $("#settings-blur").css("display", "initial");
 }
